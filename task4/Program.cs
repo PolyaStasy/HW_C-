598d4fa3c22096e1763bfd5c8d9885bc0985da9c
Polina <idaya7s@mail.ru>
2023-10-08 23:30:59 +0300

    add task 4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//  на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while(i <= number)
+{
+    System.Console.Write(i);
+    System.Console.Write("\t");
+    i = i + 2;
+}
