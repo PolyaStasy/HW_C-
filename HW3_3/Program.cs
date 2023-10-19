87fb25f78f591e51645a2ae287c17a978304de3a
Polina <idaya7s@mail.ru>
2023-10-19 15:45:41 +0300

    add HW3_3

---
+++ HW3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает Н и выдает таблицу кубов чисел от 1 до Н
+
+System.Console.WriteLine("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for(int i = 1; i<=N; i++)
+    System.Console.Write(Math.Pow(i, 3) + " ");
+
+// int i = 1;
+
+// while(i <= N)
+// {
+//     System.Console.Write(Math.Pow(i, 2) + " ");
+//     i ++;
+// }
