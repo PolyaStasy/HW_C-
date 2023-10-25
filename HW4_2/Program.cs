ff47e9d9303cf3e520c36a352b1da0feafdaf034
Polina <idaya7s@mail.ru>
2023-10-26 01:03:35 +0300

    add hw4_2

---
+++ HW4_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string text)
+{
+    System.Console.Write(text);
+    int res = Convert.ToInt32(System.Console.ReadLine());
+    return res;
+}
+
+int Sum(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число: ");
+System.Console.WriteLine(Sum(number));
