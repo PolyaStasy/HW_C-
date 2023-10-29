c65e8084531ab44fd3f47a1b971f01d92c0d004e
Polina <idaya7s@mail.ru>
2023-10-30 00:29:59 +0300

    add hw5_1

---
+++ HW5_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая подсчитывает 
+// количество четных элементов в массиве целых 
+// положительных трехзначных чисел и выводит результат 
+// на экран.
+
+int CountEvenElements(int[] array)
+{ 
+    
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+          count ++;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if(i < array.Length - 1)
+            System.Console.Write("\t");
+    }
+    Console.WriteLine();
+
+}
+
+int[] GenerateArray(int sizeArray, int leftRange, int rigthRange)
+{
+    int[] newArray = new int[sizeArray];
+    Random rand = new Random();
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = rand.Next(leftRange, rigthRange + 1);
+    }
+    return newArray;
+}
+
+// -----------------------------
+int[] array = GenerateArray(15, 100, 999);
+PrintArray(array);
+System.Console.WriteLine("Четные: " + CountEvenElements(array));
